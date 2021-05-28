7fd2c32f1ba89957b1ccf0ad681f4925a401884a
Aaznutii <aaznutii@mail.ru>
2021-05-29 03:10:03 +0400

    Курсовая работа.V_0.3

--- experiments/data_to_DB.py
+++ experiments/data_to_DB.py
@@ -1,1 +1,28 @@
+# coding=utf8
+import mysql.connector
+from sqlalchemy import create_engine
+import pandas as pd
 
+"""
+Код для импорта из таблицы эксель в БД
+"""
+
+# Ввести пароль
+passw = input()
+
+# Получить данные файла
+file_name = input()
+df = pd.read_excel(str(file_name))
+
+# Создать соединение и указать кодировку
+con = mysql.connector.connect(user='root', passwd=passw, database='ido')
+engine = create_engine(f'mysql://root:{passw}@localhost/ido?charset=utf8', echo=True)
+
+# Записать датафрейм в базу поверх данных с заменой (replace). Можно писать в конец имеющейся таблицы ('append')
+df.to_sql('countries', con=engine, if_exists='append', index=False)
+
+# Получение данных запросом и печать - контроль данных
+sql = pd.read_sql('SELECT * FROM countries', con)
+print(sql)
+
+# ПС. replace очищает всю таблицу вместе с форматами (Нужно удалаять ключи).
